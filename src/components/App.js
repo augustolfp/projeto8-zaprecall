74ac48215600581d0b00f9573f061c0591b2c185
augustolfp <augustolfp@gmail.com>
2022-05-13 15:33:27 -0300

    Organização dos arquivos-base do projeto

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../assets/styles/reset.css";
+import "../assets/styles/styles.css";
+
+export default function App() {
+    return(
+        <>
+        </>
+    );
+}
