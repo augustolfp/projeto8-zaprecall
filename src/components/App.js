3befd9772ec5e43c982099c662930306340aa826
augustolfp <augustolfp@gmail.com>
2022-05-15 19:44:37 -0300

    Criação de array de objetos com perguntas

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,40 @@ import "../assets/styles/styles.css";
 import Login from "./Login/Login";
 import GameScreen from "./GameScreen/GameScreen";
 
+
+const deck = [
+    {
+        question: "O que é JSX?",
+        answer: "Uma extensão de linguagem do JavaScript"
+    },
+    {
+        question: "O que é o React?",
+        answer: "Uma biblioteca JavaScript para construção de sites"
+    },
+    {
+        question: "Componentes devem iniciar como?",
+        answer: "Com letra maiúscula"
+    },
+    {
+        question: "O que podemos colocar dentro do JSX?",
+        answer: "Expressões!"
+    },
+    {
+        question: "O que o ReactDOM faz por nós?",
+        answer: "Interage com o DOM para colocar componentes React nele"
+    },
+    {
+        question: "Para que serve o npm?",
+        answer: "Para gerenciar pacotes e suas dependências"
+    },
+    {
+        question: "Para que servem as props?",
+        answer: "Para passar informações entre componentes"
+    }
+]
+
+
+
 export default function App() {
     return(
         <BrowserRouter>
--- src/components/QuestionBox/QuestionBox.js
+++ src/components/QuestionBox/QuestionBox.js
@@ -1,9 +1,12 @@
-    export default function QuestionBox(props) {
-        return(
-            <div className="closedQuestion">
-                <div>
-                    <span>Pergunta {props.index}</span>
-                </div>
+import "./style.css";
+import { PlayOutline } from 'react-ionicons'
+export default function QuestionBox(props) {
+    return(
+        <div className="closedQuestion">
+            <div>
+                <span>Pergunta {props.index}</span>
+                <PlayOutline width="20px" />
             </div>
-        );
-    }
+        </div>
+    );
+}
